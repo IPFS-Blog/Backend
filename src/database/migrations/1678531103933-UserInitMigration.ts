762807a395d52413fc0a0117bf6cc1199b20eb91
a20688392 <a20688392@gmail.com>
2023-04-16 00:32:24 +0800

    style: format display content

--- src/database/migrations/1678531103933-UserInitMigration.ts
+++ src/database/migrations/1678531103933-UserInitMigration.ts
@@ -5,7 +5,18 @@ export class UserInitMigration1678531103933 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`username\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`address\` varchar(255) NOT NULL, \`createAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updateAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), UNIQUE INDEX \`IDX_fe0bb3f6520ee0469504521e71\` (\`username\`), UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), UNIQUE INDEX \`IDX_b0ec0293d53a1385955f9834d5\` (\`address\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE \`users\`
+        (\`id\` int NOT NULL AUTO_INCREMENT,
+        \`username\` varchar(255) NOT NULL,
+        \`email\` varchar(255) NOT NULL,
+        \`address\` varchar(255) NOT NULL,
+        \`createAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        \`updateAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+        UNIQUE INDEX \`IDX_fe0bb3f6520ee0469504521e71\` (\`username\`),
+        UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`),
+        UNIQUE INDEX \`IDX_b0ec0293d53a1385955f9834d5\` (\`address\`),
+        PRIMARY KEY (\`id\`)
+      ) ENGINE=InnoDB`,
     );
   }
 
