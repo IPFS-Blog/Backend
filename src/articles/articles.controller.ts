9506e190bdb122fa67350ffc69e646bd971993c1
a20688392 <a20688392@gmail.com>
2023-04-15 23:42:00 +0800

    feat: add create article api swagger demo

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,9 +1,17 @@
 import { Body, Controller, Post, Request, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/auth/jwt/jwt-auth.guard";
 
 import { ArticlesService } from "./articles.service";
 import { CreateArticleDto } from "./dto/create-article.dto";
+import { CreateUnauthorizedError } from "./exceptions/create-unauthorized-error.exception";
+import { CreateArticleRespose } from "./resposes/create-article.respose";
 
 @ApiTags("Article")
 @Controller("articles")
@@ -13,6 +21,18 @@ export class ArticlesController {
   @Post()
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: "創建文章",
+    description: "將文章資訊存起來，需要 JWT 驗證",
+  })
+  @ApiCreatedResponse({
+    description: "創建成功",
+    type: CreateArticleRespose,
+  })
+  @ApiUnauthorizedResponse({
+    description: "身份驗證錯誤",
+    type: CreateUnauthorizedError,
+  })
   create(@Request() req, @Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(req.user.address, createArticleDto);
   }
