00830f9ecabca746274e8322002d5ac7f0758db2
a20688392 <a20688392@gmail.com>
2023-05-18 18:28:48 +0800

    fix: change photo to picture & add background picture

--- src/users/respose/select-username.respose.ts
+++ src/users/respose/select-username.respose.ts
@@ -13,7 +13,7 @@ export class SelectUsernameRespose {
     description: "User 資料",
     items: {
       properties: {
-        name: {
+        username: {
           description: "使用者 名稱。  \n",
           type: "string",
         },
@@ -36,7 +36,7 @@ export class SelectUsernameRespose {
       },
     },
     example: {
-      name: "Jhon",
+      username: "Jhon",
       address: "0x264D6BF791f6Be6F001A95e895AE0a904732d473",
       email: "jhon@gmail.com",
       picture: "https://i.imgur.com/Rkrp9tt.jpg",
