49d95db53adaecc111ccb46bd2cc8d52f1405534
a20688392 <a20688392@gmail.com>
2023-06-20 14:28:40 +0800

    refactor: mistaken deletion

---
+++ src/pipes/parse-int/parse-int.pipe.ts
@@ -0,0 +1,17 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from "@nestjs/common";
+
+@Injectable()
+export class ParseIntPipe implements PipeTransform<string, number> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (isNaN(value) && value !== undefined) {
+      throw new BadRequestException("無法解析為數字");
+    }
+    return value;
+  }
+}
