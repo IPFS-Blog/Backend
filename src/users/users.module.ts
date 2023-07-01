cf14224c6161bc7990b8afb8a2b6f10bafdaccab
a20688392 <a20688392@gmail.com>
2023-07-02 03:56:19 +0800

    feat: apply mail module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ArticlesModule } from "src/articles/articles.module";
+import { MailModule } from "src/mail/mail.module";
 
 import { User } from "./entities/user.entity";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
 @Module({
-  imports: [ArticlesModule, TypeOrmModule.forFeature([User])],
+  imports: [MailModule, ArticlesModule, TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
