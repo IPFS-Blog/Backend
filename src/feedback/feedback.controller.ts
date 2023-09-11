7a6aa06f625d42f1575ff16058f4b27f678481e7
a20688392 <a20688392@gmail.com>
2023-09-11 18:02:25 +0800

    feat: create a API for adding feedback

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 
+import { CreateFeedbackDto } from "./dto/create-feedback.dto";
 import { FeedbackService } from "./feedback.service";
 
 @Controller("feedback")
 export class FeedbackController {
   constructor(private readonly feedbackService: FeedbackService) {}
+  @Post("/")
+  addFeedback(@Body() feedback: CreateFeedbackDto) {
+    return this.feedbackService.addFeedback(feedback);
+  }
 }
--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { CreateFeedbackDto } from "./dto/create-feedback.dto";
+import { Feedback } from "./entities/feedback.entity";
+import { FeedbackType } from "./entities/feedback-type.entity";
 
 @Injectable()
-export class FeedbackService {}
+export class FeedbackService {
+  constructor(
+    @InjectRepository(Feedback)
+    private readonly feedbackRepository: Repository<Feedback>,
+    @InjectRepository(FeedbackType)
+    private readonly feedbackTypeRepository: Repository<FeedbackType>,
+  ) {}
+  async addFeedback(feedback: CreateFeedbackDto) {
+    const type = await this.getFeedbackType(feedback.type);
+    if (!type) {
+      throw new NotFoundException({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: "沒有此問題類型。",
+      });
+    }
+    const data = this.feedbackRepository.create({
+      email: feedback.email,
+      nickName: feedback.nickName,
+      title: feedback.title,
+      contents: feedback.contents,
+      type: type,
+    });
+    await this.feedbackRepository.save(data);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: "創建回饋成功",
+    };
+  }
+
+  async getFeedbackType(id: number) {
+    return await this.feedbackTypeRepository.findOneBy({ id: id });
+  }
+}
